14fb4dc5d196bf88303d411c2ca519d3e02e3330
eugenioLR <eugeniolorente0@gmail.com>
2024-09-23 13:51:32 +0200

    implemented multioutput objective function

--- src/evolml/feature_selection/feature_selection_mask.py
+++ src/evolml/feature_selection/feature_selection_mask.py
@@ -1,6 +1,7 @@
-from copy import copy, deepcopy
+from sklearn.base import MultiOutputMixin
 from sklearn.model_selection import RepeatedKFold
 from sklearn.metrics import r2_score
+from sklearn.multioutput import MultiOutputClassifier
 from metaheuristic_designer import ObjectiveFunc, ObjectiveVectorFunc, Encoding
 from metaheuristic_designer.initializers import UniformVectorInitializer
 from functools import reduce
@@ -70,6 +71,63 @@ class EvalFeatureSelectionMaskCV(ObjectiveVectorFunc):
         X_train_masked = self.X_train[:, vector != 0]
 
         return evaluate_model(self.baseline_model, X_train_masked, self.y_train, metric_fn=self.metric_fn, cross_validator=self.cross_validator)
+
+
+class EvalFeatureSelectionMaskMultiOutputCV(ObjectiveVectorFunc):
+    def __init__(
+        self,
+        baseline_model,
+        X_train,
+        y_train,
+        n_features,
+        target_weights=None,
+        cross_validator=None,
+        metric_fn=None,
+        random_state=None,
+    ):
+        if not isinstance(baseline_model, MultiOutputMixin):
+            baseline_model = MultiOutputClassifier(baseline_model)
+
+        self.baseline_model = baseline_model
+        self.X_train = X_train
+        self.y_train = y_train
+
+        if target_weights is None:
+            target_weights = np.full(1/y_train.shape[1], y_train.shape[1])
+        assert target_weights.shape[0] == y_train.shape[1] 
+        self.target_weights = target_weights
+
+        if cross_validator is None:
+            cross_validator = RepeatedKFold(n_splits=5, n_repeats=10)
+
+        if hasattr(cross_validator, "random_state"):
+            cross_validator.random_state = random_state
+
+        self.cross_validator = cross_validator
+
+        if metric_fn is None:
+            metric_fn = r2_score
+        self.metric_fn = metric_fn
+
+        self.random_state = random_state
+        n_base_features = reduce(lambda x, y: x * y, X_train.shape[1:])
+
+        super().__init__(
+            n_features,
+            mode="max",
+            low_lim=0,
+            up_lim=n_base_features - 1,
+            name="Evaluate Masked Feature Selection",
+        )
+
+    def objective(self, vector):
+        X_train_masked = self.X_train[:, vector != 0]
+
+        metrics = np.empty(self.y_train.shape[1])
+        for idx, target_i in enumerate(self.y_train.T):
+            metrics[idx] = evaluate_model(self.baseline_model, X_train_masked, target_i, metric_fn=self.metric_fn, cross_validator=self.cross_validator)
+        
+        return np.average(metrics, weights=self.class_weights)
 
 
 def select_features(
