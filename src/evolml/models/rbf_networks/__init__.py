69826775787eaba73c7a4c70f19e398f7712da35
eugenioLR <eugeniolorente0@gmail.com>
2025-01-23 16:02:00 +0100

    reorganized code

--- src/evolml/models/rbf_networks/__init__.py
+++ src/evolml/models/rbf_networks/__init__.py
@@ -1,3 +1,2 @@
 from .rbf_layer import RBFLayer
-from .rbf_classifier import RBFNNClassifier
-from .rbf_regressor import RBFNNRegressor
+from .rbf_model import RBFNNClassifier, RBFNNRegressor
--- src/evolml/models/rbf_networks/rbf_model.py
+++ src/evolml/models/rbf_networks/rbf_model.py
@@ -1,16 +1,17 @@
 from __future__ import annotations
+from abc import ABC, abstractmethod
 from typing import Tuple
 import torch
 import scipy as sp
 import numpy as np
-from sklearn.linear_model import LogisticRegression, RidgeClassifier
+from sklearn.linear_model import LogisticRegression, RidgeClassifier, LinearRegression
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
 from sklearn.cluster import KMeans
 
 
-class RBFNNModel(BaseEstimator, ClassifierMixin):
+class RBFNNModel(ABC, BaseEstimator, ClassifierMixin):
     """ """
 
     def __init__(
@@ -99,3 +100,57 @@ class RBFNNModel(BaseEstimator, ClassifierMixin):
         X_rbf = self._rbf_layer(X, self.centers_, self.widths_)
 
         return self.linear_layer.predict_proba(X_rbf)
+
+
+class RBFNNClassifier(RBFNNModel):
+    """ """
+
+    def __init__(
+        self,
+        n_units: int,
+        probability: float = True,
+        linear_layer: BaseEstimator = None,
+        cluster_model: BaseEstimator = None,
+        std_from_clusters: bool = False,
+        random_state: int = None,
+    ):
+        self.probability = probability
+
+        if linear_layer is None:
+            if probability:
+                linear_layer = LogisticRegression(penalty=None, random_state=random_state)
+            else:
+                linear_layer = RidgeClassifier(alpha=0, solver="svd", random_state=random_state)
+
+        super().__init__(
+            n_units=n_units,
+            linear_layer=linear_layer,
+            cluster_model=cluster_model,
+            std_from_clusters=std_from_clusters,
+            classification=True,
+            random_state=random_state,
+        )
+
+
+class RBFNNRegressor(RBFNNModel):
+    """ """
+
+    def __init__(
+        self,
+        n_units: int,
+        linear_layer: BaseEstimator = None,
+        cluster_model: BaseEstimator = None,
+        std_from_clusters: bool = False,
+        random_state: int = None,
+    ):
+        if linear_layer is None:
+            linear_layer = LinearRegression()
+
+        super().__init__(
+            n_units=n_units,
+            linear_layer=linear_layer,
+            cluster_model=cluster_model,
+            std_from_clusters=std_from_clusters,
+            classification=False,
+            random_state=random_state,
+        )
